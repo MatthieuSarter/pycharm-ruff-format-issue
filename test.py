ceb52953cc0c39714d98271cb6cb224d68fdebb7
Matthieu Sarter <matthieu@sarter.fr>
2025-04-02 19:23:18 +0200

    init

---
+++ test.py
@@ -0,0 +1,17 @@
+
+
+class BadlyFormattedClass:
+
+
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+
+
+    def method_one(self):
+        print(f"Method one: {self.arg1}") 
+
+
+    def method_two(self):
+        print(f"Method two: {self.arg2}")
